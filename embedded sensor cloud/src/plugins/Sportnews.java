9d458adcb15cf86fd8e042fa1eaf548972ee546f
StefanZ <if12b031@technikum-wien.at>
2014-01-08 15:28:53 +0100

    sportnews plugin added(renamed bestPluginWorld)

--- embedded sensor cloud/src/plugins/Sportnews.java
+++ embedded sensor cloud/src/plugins/Sportnews.java
@@ -5,16 +5,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 
+import server.Feed;
+import server.FeedMessage;
+import server.FeedParser;
 import server.Plugin;
 import server.PluginManager;
 
-public class BestPluginWorld implements Plugin {
+public class Sportnews implements Plugin {
 
 	@Override
 	public String execPlugin(String param, Socket socket) {
 		String response = "";
 		
 		response = prepareResponse();
+		
+		
+		FeedParser parser = new FeedParser("http://feeds.reuters.com/reuters/sportsNews");
+	    Feed feed = parser.readFeed();
+	    for (FeedMessage message : feed.getMessages()) {
+	      response += message;
+	    }
 		
 		return response;
 	}
